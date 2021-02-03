7e1f2749afa1f0eedd15573a1b7ef450c43b6a72
srzotova <sophyzotova@yandex.ru>
2021-02-03 23:16:29 +0300

    add constants, add cities and cities_graph

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
-def main():
-    print('first commit')
-    print('I just try to change it)')
+import csv
+import pygame
+import os
+import sys
+import queue
+
+MAK_CONTAMINATION = 4
+IMAGE_W = 1357
+IMAGE_H = 628
+VIRUS_COLORS = [(10, 10, 10), (0, 0, 255), (255, 255, 0), (255, 0, 0)]
+CITY_RADIUS = 10
 
 
-if __name__ == '__main__':
-    main()
+def load_cities():
+    cities = []
+    with open('cities.csv', encoding="utf8") as csvfile:
+        reader = csv.reader(csvfile, delimiter=';', quotechar='"')
+        for record in reader:
+            num = int(record[0]) - 1
+            name = record[1]
+            cords = (int(record[2]), int(record[3]))
+            virus = int(record[4]) - 1
+            cities.append((num, name, cords, virus))
+    return cities
+
+
+def load_cities_graph():
+    graph = []
+    with open('graph.csv', encoding="utf8") as csvfile:
+        reader = csv.reader(csvfile, delimiter=';', quotechar='"')
+        for record in reader:
+            c_1 = int(record[0]) - 1
+            c_2 = int(record[1]) - 1
+            graph.append((c_1, c_2))
+    return graph
