d07f082575c4d2cc1135a267a3f17bc46a267d0f
ArtemTeleluhin <teleluhin.artem@yandex.ru>
2021-02-04 19:33:17 +0300

    Добавлен класс Player, работающий с информацией об игроке, и многие игровые механики. Добавлены колоды игроков и заражений

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ import pygame
 import os
 import sys
 import queue
+from random import shuffle
 
+# параметры игровых механик
 MAK_CONTAMINATION = 4
+INFECTION_CARD_NAME = 'Заражение'
+INFECTION_CARDS_COUNT = 6
+HOW_TAKE = 2
+# параметры рисовки
 IMAGE_W = 1357
 IMAGE_H = 628
 VIRUS_COLORS = [(10, 10, 10), (0, 0, 255), (255, 255, 0), (255, 0, 0)]
@@ -77,6 +83,12 @@ class Town:
         self.contamination += 1
         return True
 
+    def medication(self):
+        if self.contamination == 0:
+            return False
+        self.contamination -= 1
+        return True
+
     def take_contamination(self):
         return self.contamination
 
@@ -88,6 +100,39 @@ class Town:
 
     def take_neighbors(self):
         return self.neighbors
+
+
+class Player:
+    def __init__(self, num, role, location):
+        self.num = num
+        self.role = role
+        self.location = location
+        self.hand = []
+
+    def take_location(self):
+        return self.location
+
+    def set_location(self, location):
+        self.location = location
+
+    def take_role(self):
+        return self.role
+
+    def take_num(self):
+        return self.num
+
+    def add_card(self, card):
+        self.hand.append(card)
+
+    def del_card(self, card):
+        index = self.hand.index(card)
+        if index == -1:
+            return False
+        del self.hand[index]
+        return True
+
+    def take_hand(self):
+        return self.hand
 
 
 class Game:
@@ -102,6 +147,14 @@ class Game:
             name_1, name_2 = cities_list[c_1][1], cities_list[c_2][1]
             self.cities[name_1].add_neighbor(self.cities[name_2])
             self.cities[name_2].add_neighbor(self.cities[name_1])
+
+        cities_names = [city[1] for city in cities_list]
+        cards = cities_names + [INFECTION_CARD_NAME] * INFECTION_CARDS_COUNT
+        shuffle(cards)
+        self.players_pack = iter(cards)
+        cards = cities_names * (MAK_CONTAMINATION + 1)
+        shuffle(cards)
+        self.infection_pack = iter(cards)
 
     def get_element(self, x, y):
         for city in self.cities.values():
@@ -122,3 +175,22 @@ class Game:
                     used[neig.take_num()] = True
                     if not neig.infection():
                         infected.put(neig)
+
+    def move_player(self, player, city):
+        player.take_location().del_player(player)
+        city.add_player(player)
+        player.set_location(city)
+
+    def open_players_card(self):
+        return next(self.players_pack)
+
+    def open_infections_card(self):
+        return next(self.infection_pack)
+
+    def receiving_cards(self, player):
+        for _ in range(HOW_TAKE):
+            player.add_card(self.open_players_card())
+
+    def transfer_card(self, player_from, player_to, card):
+        if player_from.del_card(card):
+            player_to.add_card(card)
